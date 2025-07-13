3f6c3d690cda2637118ffddb25e3b2034e8bac8a
Alvinus Melius <37239944+amello69@users.noreply.github.com>
2025-07-13 17:22:50 -0400

    Update profile.py
    define new run function based on calls in streamlit_app.py

--- pages/profile.py
+++ pages/profile.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-if __name__ == "__page__":
+def run():
     st.title("👤 Your Profile")
-
     username = st.session_state.get("logged_in_user", "")
     name = st.session_state.get("logged_in_name", "")
-    
     st.write(f"Welcome, **{name}** (username: `{username}`)!")
