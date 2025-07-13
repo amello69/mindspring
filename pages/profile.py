5c8c3d06de11d58f98a0819e1e9c9f10ba8d8cc3
Alvinus Melius <37239944+amello69@users.noreply.github.com>
2025-07-13 17:12:33 -0400

    Update profile.py
    now getting the copied output from the authenticator (user, username)

--- pages/profile.py
+++ pages/profile.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-def run():
-    st.title("👤 Your Profile")
-    username = st.session_state.get("username", "")
-    name = st.session_state.get("name", "")
-    st.write(f"Welcome, **{name}** (username: `{username}`)!")
-
 if __name__ == "__page__":
-    run()
+    st.title("👤 Your Profile")
+
+    username = st.session_state.get("logged_in_user", "")
+    name = st.session_state.get("logged_in_name", "")
+    
+    st.write(f"Welcome, **{name}** (username: `{username}`)!")
