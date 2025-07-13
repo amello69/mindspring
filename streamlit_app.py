2c024ebddce77d4419db8a5a79892813add7ffc9
Alvinus Melius <37239944+amello69@users.noreply.github.com>
2025-07-13 18:57:54 -0400

    Update streamlit_app.py
    add code to initialize tokens

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ if status is True:
     if "logged_in_user" not in st.session_state:
         st.session_state["logged_in_user"] = st.session_state.get("username", "")
         st.session_state["logged_in_name"] = st.session_state.get("name", "")
+        st.session_state["monthly_tokens"] = 1000
+        st.session_state["tokens_remaining"] = 1000
     st.success(f"Welcome, {st.session_state['logged_in_name']}!")
 elif status is False:
     st.error("Incorrect username/password")
